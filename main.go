24ba280b20d5fa16a3eb13ad49ab4545837650fe
illinois <il.koval42@gmail.com>
2023-06-25 22:11:29 +0700

    air tools

--- main.go
+++ main.go
@@ -1,28 +1,41 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"fmt"
 	"go-api-app/app/auth"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-		return
+		os.Exit(1)
+	}
+
+	DEV, err := strconv.ParseBool(os.Getenv("DEV_MODE"))
+	if err == nil {
+		if !DEV {
+			gin.SetMode(gin.ReleaseMode)
+		}
 	}
 
 	router := gin.Default()
 	auth.Routes(router)
 
+	PORT := os.Getenv("PORT")
 	server := &http.Server{
-		Addr:         ":5001",
+		Addr:         fmt.Sprintf(":%s", PORT),
 		Handler:      router,
 		WriteTimeout: 10 * time.Minute,
 	}
+	fmt.Printf("Server starting on port: %s\n", PORT)
 	server.ListenAndServe()
 }
